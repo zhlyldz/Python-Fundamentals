2ef43987bd5b68999a35442ceb18cf807679637f
Zuhal YILDIZ <zuhal_yildiz@hotmail.com>
2023-05-09 19:24:49 +0300

    Python soru örnekleri

---
+++ Python genel soru çözümleri/Python genel soru çözümleri.py
@@ -0,0 +1,85 @@
+#Kullanıcının girdiği 2 sayı arasındaki çift sayıların ortalamasını bulan Python örneği.
+"""
+sayi_1=int(input("sayi 1: "))
+sayi_2=int(input("sayi 2: "))
+if sayi_1<sayi_2 and sayi_1>0:
+  lst=[]
+  for i in range(sayi_1, sayi_2):
+      if i %2 == 0:
+          lst.append(i)
+  print(sum(lst)/len(lst))
+else:
+  print("Lütfen doğru sayı giriniz")
+"""
+"""
+#1-100 Arası 3′ e ve 5′ e tam bölünen sayıları bulan ve kaç tane olduğunu gösteren uygulama yapınız
+# list=[]
+# for i in range(1,101):
+#     if i%3==0 and i%5==0:
+#       list.append(i)
+#       print(i)
+#print(len(list))
+"""
+
+#Python ile bir liste içinde 5’in katları olan sayıları listeleme 1-10000
+"""
+
+list=[]
+for i in range (1,10000):
+   if i %5 ==0:
+     list.append(i)
+print(list)
+
+"""
+
+#Bir sayının kendisi dışında bütün pozitif bölenlerinin toplamı kendisine eşit olan sayılara mükemmel sayı denir.
+# Kullanıcıdan alınan sayının mükemmel sayı olup olmadığını kontrol eden kodu yazınız.
+"""
+lst=[]
+sayi=int(input("Bir sayı giriniz:"))
+for i in range(1, sayi):
+    if sayi % i == 0:
+        lst.append(i)
+if sum(lst) == sayi:
+    print(f"{sayi} mükemmel sayıdır")
+else:
+    print(f"{sayi} mükemmel sayı değildir")
+"""
+
+#Bir string içerisinde belirlenen bir karakterin olup olmadığını kontrol eden Python programı kodları.
+"""
+metin=input("metin: ")
+for i in metin:
+   if i == "k":
+    print("karakter vardır")
+    break
+   else:
+       print("Karakter yoktur")
+       break
+"""
+#Kullanıcıya sinema ya da tiyatro tercihi sorulsun. Sinema izlemek için 15 TL, tiyatro için 10 TL ödenmesi gerekmedir
+#Öğrencilere %50 indirim yapıldığı düşünülerek öğrenci ise indirim yapılan; öğrenci değilse indirimsiz tutarı#
+#hesaplayarak ekrana yazdıran kodu yazınız.
+"""
+aktivite=input("aktivite=")
+kisi=input("kisi=")
+
+if aktivite == "sinema":
+   if kisi == "öğrenci":
+      print(15-15*0.5)
+   else:
+      print(15)
+if aktivite == "tiyatro":
+   if kisi=="öğrenci":
+      print(10-10*0.5)
+   else:
+   print(10)
+"""
+#Girilen metnin harflerini alt alta yazdıran Python Örneği
+"""
+metin=input("metin=")
+for i in metin:
+   print(i)
+"""
+
+
---
+++ Udemy Python Soru Çözümleri/udemy örnekler.py
@@ -0,0 +1,72 @@
+#Example-1
+#ismi Çağrı soyismi Güngör doğum tarihi 1992 doğum yeri Ankara yaşadığı yer Odessa  olan birinin bilgilerinden bir cümle
+# oluşturma
+"""
+isim="Çağrı"
+soy_isim="Güngör"
+dogum_tarihi=1992
+dogum_yeri="Ankara"
+yasadigi_yer="Odessa"
+print(f"{isim} {soy_isim} {dogum_tarihi} tarihinde {dogum_yeri}da doğmustur. Şuan {yasadigi_yer}'da yaşamaktadır.")
+"""
+"""
+#input ile girilen bir ismin büyük ünlü uyumuna uyup uymadığını kontrol eden bir program yazınız
+
+isim=input("isim: ")
+sesli_harf=["a","ı","o","u","e","i","ö","ü"]
+kalin_unlu=["a","ı","o","u"]
+ince_unlu=["e","i","ö","ü"]
+x=0
+y=0
+for harf in isim:
+    if harf in kalin_unlu:
+        x+=1
+    elif harf in ince_unlu:
+        y+=1
+if x == 0 or y == 0:
+    print("Büyük ünlü uyumu ile uyumlu")
+else:
+    print("Büyük ünlü uyumu ile uyumsuz")
+"""
+#kullanıcı adının admin sifrenin TC1923 olduğu bir sifreyi kontrol eden şifre doğruysa sifre basarılı değilse sifre
+# basarısız yazan ve sifreyi tekrar soran bir program yazınız
+"""
+kullanici_adi=input("isim: ")
+
+if kullanici_adi== "admin":
+    while True:
+        sifre = input("sifre: ")
+        if sifre == "TC1923":
+            print("Başarılı..!")
+            break
+        else:
+            print("Başarısız...! Şifreniz yanlış")
+else:
+    print("Başarısız..! Kullanıcı adınız yanlış")
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
