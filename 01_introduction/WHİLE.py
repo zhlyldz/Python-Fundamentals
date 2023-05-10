96b92a261293c31cc60e150e45f8d08008aa3cd2
Zuhal YILDIZ <zuhal_yildiz@hotmail.com>
2023-05-10 23:44:21 +0300

    Update WHİLE.py

--- 01_introduction/WHİLE.py
+++ 01_introduction/WHİLE.py
@@ -1,16 +1,16 @@
-#DÖNGÜLERRR
+DÖNGÜLER
 
-"""
-#0-100 ARASINDAKİ SAYILARI YAZIDRALIM.LOOPLARDA SAYAC MANTIĞI VAR.AŞAĞIDAKİ ÇÖZÜM HOCANIN.(DERS)
+Example-1
+
+0-100 arasındaki sayıları yazdıralım
 sayac=0
 while <=100:
     print(sayac)
     sayac+=1
 
-"""
 
-#Soru:1-101 arasında kaç tane çift kaç tane tek sayı var ayrı ayrı bulalım
-"""
+Soru:1-101 arasında kaç tane çift kaç tane tek sayı var ayrı ayrı bulalım
+
 sayac_cift=0
 sayac_tek=0
 a=1
@@ -23,10 +23,8 @@ while  a <=101:
         a+=1
 print(sayac_cift)
 print(sayac_tek)
-"""
 
-#Soru: 1 ile 100 arasındaki çift ve tek sayıların ayrı ayrı toplamlarını yazdıralım
-"""
+Soru:2 ile 100 arasındaki çift ve tek sayıların ayrı ayrı toplamlarını yazdıralım
 
 cift_toplam=0
 tek_toplam=0
@@ -40,13 +38,11 @@ while sayac<=101:
         sayac+=1
 print(tek_toplam)
 print(cift_toplam)
-"""
 
-#Soru: 1 ile 101 ARASINDAKİ TEK VE ÇİFT SAYILARIN MİKTARINI YAZAN UYGULAMA YAPALIM (DERS)
-"""
+Soru: 1 ile 101 arasındaki tek ve çift sayıların miktarını yazan uygulama yapalım
 cift_miktar=0
 tek_miktar=0
-sayac=1     #SAYAC 1 ÇÜNKÜ SORUDA 1-101 ARASINDAKİ SAYILAR İSTENMİŞ SAYAC BU ARALIKTAKİ SAYILARI TEK TEK ZİYARET EDECEK
+sayac=1
 
 while sayac<= 100:
     if sayac %2 ==0:
@@ -57,14 +53,11 @@ while sayac<= 100:
     sayac+=1
 print(f' {cift_miktar} tane çift sayı vardır' )
 print(f'{tek_miktar} tane tek miktar vardır ')
-"""
-"""
-#1-1000 arasındaki çift ve tek sayıların toplamlarını yazdıralım(HOCA-DERS)
 
+1-1000 arasındaki çift ve tek sayıların toplamlarını yazdıralım
 cift_toplam=0
 tek_toplam=0
-sayac=1            #SAYACIN İF BLOKLARINDAN AYRI ARTMASINI İSTİYORUM.BAĞIMSIZ OLARAK ARTSIN.HER İKİ BLOKTA
-                   #GEREKMİYOR. WHİLEN ALTINDA İF VE ELSE BLOKLARI İLE AYNI HİZADA SAYACI ARTIRMALISIN!!(DERS)
+sayac=1
 
 while sayac<=1000:
     if sayac %2==0:
@@ -77,17 +70,10 @@ while sayac<=1000:
 print(tek_toplam)
 print(cift_toplam)
 
+Soru: Kullanıcıdan bir işlem sembolü alalım (+,- vb).Bu seçilen işlem sembolü üzerinden kullanıcının girdiği 2 sayı
+ile aritmatiksel işlem yapalım.Kullanıcı işlem sembolü seçmek yerine "e " harfi girerse uygulama dursun.
+Kullanıcı isteği kadar işlem yapabilsin
 
-
-
-
-
-
-#Soru: Kullanıcıdan bir işlem sembolü alalım (+,- vb).Bu seçilen işlem sembolü üzerinden kullanıcının girdiği 2 sayı
-# ile aritmatiksel işlem yapalım.Kullanıcı işlem sembolü seçmek yerine "e " harfi girerse uygulama dursun.
-# Kullanıcı isteği kadar işlem yapabilsin
-"""
-"""
 while True:
     sayi_1= int(input("Bir sayı giriniz: "))
     sayi_2= int(input("Bir sayı giriniz: "))
@@ -110,9 +96,8 @@ while True:
 
     else:
         print("Doğru işlem giriniz")
-"""
-"""
-#DAHA KISA YOLU AYNI İŞLEM AMA KISA YOLU (DERS)
+
+2.yol;
 
 while True:
     
@@ -135,15 +120,10 @@ while True:
             print(f'Sonuç: {sayi_1 / sayi_2}')
         else:
             print(f'Doğru işlem bilgisi giriniz')
-"""
 
 
 
-
-
-
-"""
-#1-101 arasındaki tek sayıların toplamı
+1-101 arasındaki tek sayıların toplamı
 
 sayac=1
 toplam=0
@@ -152,11 +132,10 @@ while sayac<102:
         toplam=toplam+sayac
     sayac+=1
 print(toplam)
-"""
-"""
-# İN OPARETÖRÜ İLE BİR LİSTE İÇERİSİNDE İTEM VARSA TRUE YOKSA FALSE DÖNER.
-# YANİ BURADA KULLANICININ GİRSİĞİ İŞLEM BİZİM İŞLEM TÜRÜ LİSTENİZDE
-# VARSA tRUE YOKSA FALSE DÖNECEK (HOCAA)
+
+İN OPARETÖRÜ İLE BİR LİSTE İÇERİSİNDE İTEM VARSA TRUE YOKSA FALSE DÖNER.
+YANİ BURADA KULLANICININ GİRSİĞİ İŞLEM BİZİM İŞLEM TÜRÜ LİSTENİZDE
+VARSA tRUE YOKSA FALSE DÖNECEK
 
 prosess_type_list=['+', '-', '*', '/','e']
 
@@ -180,40 +159,35 @@ while True:
     else:
         print("Doğru işlem giriniz")
 
-"""
+1950 yılı ile günümüz arasındaki  kullanıcıdan bir yıl bilgisi alalım şayet 1950 ve günümüz arasında ise "Buldunuz"
+Değilse bulamadık diye feedback veren uygulama yaz
 
-#1950 yılı ile günümüz arasındaki  kullanıcıdan bir yıl bilgisi alalım şayet 1950 ve günümüz arasında ise "Buldunuz"
-# Değilse bulamadık diye feedback veren uygulama yaz
-"""
 yil=int(input("Lütfen yıl bilgisi giriniz: "))
 if 1950<=yil<=2023:
   print("Buldunuz")
 else:
   print("Bulamadık")
-"""
 
-#KULLANICIDAN YIL BİLGİSİ ALACAĞIZ BU YIL BİLGİSİ 1943 DEĞİLSE ARADIĞINIZ YIL BULUNAMMAKTADIR UYGULAMASI YAZIN(DERS)
-#ÖNEMLİ!!
+KULLANICIDAN YIL BİLGİSİ ALACAĞIZ BU YIL BİLGİSİ 1943 DEĞİLSE ARADIĞINIZ YIL BULUNAMMAKTADIR UYGULAMASI YAZIN
 
-"""
 from datetime import datetime 
 
 started_date =1943
 search_date =int (input("aradığınız yılı girin: "))
 is_exist=False
 
-while started_date <= datetime.now().year:    #baştakini yapabilmek için bunu yazdık günümüz yılını 2023 olarak değilde 
-    if started_date == search_date:           #now olarak yazdık
+while started_date <= datetime.now().year:
+    if started_date == search_date:
         print("Buldunuz")
         is_exist = True
         break
 
     started_date+=1
 
-if not is_exist:  # true değilse demek tani false ise yıkardaki gibi yukarda biz değişkene true yaptık.Tutmadığı zamanlarda
+if not is_exist:
     print("Aradınığınız tarih bulunamadı...")
-        
-BENİMKİ;
+
+2.yol;
 
 yil=int(input("yıl: "))
 sayac=1943
@@ -227,10 +201,8 @@ while sayac<=2023:
 if not is_exist:
     print("BulUnamadı")
 
-"""
+0-101 arası tek sayıların toplamını continue kullanarak bul
 
-#0-101 arası tek sayıların toplamını bul continue kullanarak
-"""
 sayac=0
 toplam=0
 
@@ -243,13 +215,9 @@ while sayac<= 101:
 
 print(toplam) 
 
-"""
+Kullanıcıdan başlangıç bitiş ve adım miktarları alarak bir loop kuralım.
+Kullanıcının istediği adım miktarına göre sayıları toplatarak ekrana yazdıralım.
 
-
-#Kullanıcıdan başlangıç bitiş ve adım miktarları alarak bir loop kuralım.
-# Kullanıcının istediği adım miktarına göre sayıları toplatarak ekrana yazdıralım. HATAAAA
-
-"""
 baslangic= int(input("Başlangıc= "))
 bitis= int (input("Bitiş:"))
 adim = int (input( "adım miktarı: "))
@@ -259,12 +227,11 @@ while baslangic<bitis:
     baslangic=baslangic+adim
     toplam=baslangic+baslangic+adim
 print(toplam)
-"""
-#Sayı tahmin oyunu 1-100 arasında rastgele bir sayı üretildiğini düşünelim.Kullanıcı bu üretilen sayıyı 3 hakta tahmin
-# etmeye çalışacak.Kullanıcı üretilen sayıdan daha büyük  bir sayı girerse daha küçük bir sayı giriniz ya da üretilen
-# sayıdan daha büyük sayı giriniz şeklinde yönlendireceğiz #HAAAATAAA
 
-"""
+Sayı tahmin oyunu 1-100 arasında rastgele bir sayı üretildiğini düşünelim.Kullanıcı bu üretilen sayıyı 3 hakta tahmin
+etmeye çalışacak.Kullanıcı üretilen sayıdan daha büyük  bir sayı girerse daha küçük bir sayı giriniz ya da üretilen
+sayıdan daha büyük sayı giriniz şeklinde yönlendireceğiz
+
 uretilen_sayi=int(input("üretilen sayi= "))
 if 1<= uretilen_sayi<=100:
     hak = 3
@@ -282,10 +249,10 @@ if 1<= uretilen_sayi<=100:
         print("bulamadınız")
 else:
     print("aralıkta seçiniz")
-"""
 
-#Kullanıcıdan alınan sayının asal olup olmadığını ekrana yazdıralım
-"""
+
+Kullanıcıdan alınan sayının asal olup olmadığını ekrana yazdıralım
+
 sayi = int(input("Bir sayı giriniz: "))
 if 1<sayi:
     bolen = 2
@@ -301,7 +268,7 @@ if 1<sayi:
             print("Asal değildir")
 else:
     print("asal değildir")
-"""
+
 
 
 
