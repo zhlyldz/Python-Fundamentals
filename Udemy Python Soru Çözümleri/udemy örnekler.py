a828733b8e512a8c2d9e458873d88c8ba0376b7d
Zuhal YILDIZ <zuhal_yildiz@hotmail.com>
2023-05-11 00:19:23 +0300

    Update udemy örnekler.py

--- Udemy Python Soru Çözümleri/udemy örnekler.py
+++ Udemy Python Soru Çözümleri/udemy örnekler.py
@@ -1,16 +1,15 @@
-#Example-1
-#ismi Çağrı soyismi Güngör doğum tarihi 1992 doğum yeri Ankara yaşadığı yer Odessa  olan birinin bilgilerinden bir cümle
-# oluşturma
-"""
+Example-1
+ismi Çağrı soyismi Güngör doğum tarihi 1992 doğum yeri Ankara yaşadığı yer Odessa  olan birinin bilgilerinden bir cümle
+oluşturma
+
 isim="Çağrı"
 soy_isim="Güngör"
 dogum_tarihi=1992
 dogum_yeri="Ankara"
 yasadigi_yer="Odessa"
 print(f"{isim} {soy_isim} {dogum_tarihi} tarihinde {dogum_yeri}da doğmustur. Şuan {yasadigi_yer}'da yaşamaktadır.")
-"""
-"""
-#input ile girilen bir ismin büyük ünlü uyumuna uyup uymadığını kontrol eden bir program yazınız
+
+input ile girilen bir ismin büyük ünlü uyumuna uyup uymadığını kontrol eden bir program yazınız
 
 isim=input("isim: ")
 sesli_harf=["a","ı","o","u","e","i","ö","ü"]
@@ -27,10 +26,10 @@ if x == 0 or y == 0:
     print("Büyük ünlü uyumu ile uyumlu")
 else:
     print("Büyük ünlü uyumu ile uyumsuz")
-"""
-#kullanıcı adının admin sifrenin TC1923 olduğu bir sifreyi kontrol eden şifre doğruysa sifre basarılı değilse sifre
-# basarısız yazan ve sifreyi tekrar soran bir program yazınız
-"""
+
+kullanıcı adının admin sifrenin TC1923 olduğu bir sifreyi kontrol eden şifre doğruysa sifre basarılı değilse sifre
+basarısız yazan ve sifreyi tekrar soran bir program yazınız
+
 kullanici_adi=input("isim: ")
 
 if kullanici_adi== "admin":
@@ -43,7 +42,7 @@ if kullanici_adi== "admin":
             print("Başarısız...! Şifreniz yanlış")
 else:
     print("Başarısız..! Kullanıcı adınız yanlış")
-"""
+
 
 
 
